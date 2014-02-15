ea67ad17d104331825220a09e59043607801fce4
Fausto Spoto <fausto.spoto@juliasoft.com>
2014-02-15 22:34:03 +0100

    Modified the obfuscation script

--- scripts/obfuscate.sh
+++ scripts/obfuscate.sh
@@ -5,7 +5,7 @@
 cd bin
 time java -Xmx1000m com.juliasoft.beedeedee.examples.MultiQueens 10 9 8
 jar -cf beedeedee.jar com/*
-java -Xmx1000m -classpath ../../julia-annotations/bin:../../julia-core/bin:../../julia-core/lib/bcel-5.2.jar:../../julia-core/lib/android.jar:../../julia-core/lib/javabdd-1.0b2.jar com.juliasoft.julia.engine.Julia -si beedeedee.jar -i java.lang. -jb -outputPath obfuscated -framework java -Obfuscator where alias kind loopOnNop
+java -Xmx1000m -classpath ../../julia-annotations/bin:../../julia-core/bin:../../julia-core/lib/bcel-5.2.jar:../../julia-core/lib/android.jar:../../julia-core/lib/javabdd-1.0b2.jar com.juliasoft.julia.engine.Julia -si beedeedee.jar -i java.lang. -jb -outputPath obfuscated -framework java -Obfuscator where expAlias kind loopOnCalculations
 cd obfuscated
 time java -Xmx1000m com.juliasoft.beedeedee.examples.MultiQueens 10 9 8
 cd ..
