80fedd6f12e842071674ac4b075b9f279503045d
Alberto Lovato <alberto.lovato@univr.it>
2016-08-30 14:08:39 +0200

    Revert "Small optimization"
    This reverts commit b1f73167febb4c1aad2b4c0df9685dea480a1f3e.

--- src/com/juliasoft/beedeedee/factories/EquivalenceRelation.java
+++ src/com/juliasoft/beedeedee/factories/EquivalenceRelation.java
@@ -464,17 +464,14 @@ public class EquivalenceRelation implements Iterable<BitSet> {
 	 * @return the minimum leader >= c, or -1 if it does not exist
 	 */
 	public int getMinLeaderGreaterOrEqualtTo(int c, int var, Filter filter) {
+		int min = -1;
 		for (BitSet eqClass: equivalenceClasses) {
 			int leader = eqClass.nextSetBit(0);
-			// the following logic is correct since bitsets are kept in order
-			// wrt their leftmost bit set
-			if (leader >= var)
-				return -1;
-			else if (leader >= c && filter.accept(eqClass))
-				return leader;
+			if (leader >= c && (min < 0 || leader < min) && leader < var && filter.accept(eqClass))
+				min = leader;
 		}
 	
-		return -1;
+		return min;
 	}
 
 	public static interface Filter {
